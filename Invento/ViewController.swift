bca3594365888b1e173d7dd5f2d503f0e4e93dc7
Adhitya Surya Pratama <adhityasuryapratama@yahoo.com>
2018-03-02 21:44:50 +0700

    Initial Commit

---
+++ Invento/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Invento
+//
+//  Created by Adhitya Surya Pratama on 02/03/18.
+//  Copyright © 2018 Adhitya Surya Pratama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
